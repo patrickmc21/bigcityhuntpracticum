933fda695717bf2fb4768bdee7ffc3017c8d34f3
Patrick McLaughlin <patrickmc21@gmail.com>
2018-07-01 15:50:29 -0600

    Add modal

--- containers/MapContainer.js
+++ containers/MapContainer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {
-  View, 
+  View,
+  Modal,
+  Text,
+  Button,
   StyleSheet,
   Platform,
   Dimensions
 } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { connect } from 'react-redux';
 
 const deviceSize = Dimensions.get('window');
@@ -18,34 +21,86 @@ class MapContainer extends Component<Props> {
       longitude: -98.5795,
       latitudeDelta: 20,
       longitudeDelta: 30,
-    }
+    },
+    showModal: false,
+    selectedHunt: {}    
   };
 
+  handlePress = (hunt) => {
+    this.setState({ showModal: true, selectedHunt: hunt });
+  }
+
   render() {
+    const { selectedHunt } = this.state;
     const markers = this.props.hunts.map(hunt => {
-      return (
+      return (        
         <Marker 
           key={hunt.key}
           coordinate={{latitude: parseFloat(hunt.lat), longitude: parseFloat(hunt.long)}}
-        />
+          onPress={() => this.handlePress(hunt)}
+        />                      
       )
     })
     return (
+      <View style={styles.container}>
       <MapView
         style={styles.map}
         region={this.state.region}
       >
        {markers}
       </MapView>
+        <View styles={styles.wrapper}>
+          <Modal
+            animationType='slide'
+            transparent={true}
+            visible={this.state.showModal}
+            onRequestClose={() => {
+              alert('Modal has been closed.');
+            }}              
+          >
+            <View style={styles.modal}>
+                <Text style={styles.title}>
+                  {this.state.selectedHunt.name}
+                </Text>
+                <Button
+                  title="Hide Modal"
+                  onPress={() => {
+                    this.setState({ showModal: false});
+                }}/> 
+            </View>                 
+            </Modal>                       
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: (deviceSize.height / 8) * 6,
+    width: deviceSize.width,
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
     height: (deviceSize.height / 8) * 6,
     width: deviceSize.width,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  modal: {
+    marginTop: deviceSize.height / 8,
+    height: (deviceSize.height / 8) * 6,
+    width: deviceSize.width,
+    backgroundColor: '#FEFEFE'
+  },
+  title: {
+    fontSize: 30,
+    color: '#000000',
+    textAlign: 'center'
   }
 })
 
